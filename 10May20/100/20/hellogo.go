4150e13fa9442399d2cb32dc7f84c2b4938d2349
Pradeep Gandla <pradeepdoit@gmail.com>
2020-05-12 00:47:48 +0530

    nothing much

--- 10May20/100/20/hellogo.go
+++ 10May20/100/20/hellogo.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+//Outside main use var
+//Keep least scope definition
+//Types...
+type coffy int
+var c coffy
+
+func main() {
+	c = 12
+	a := 23
+	fmt.Printf("%T, %v\n", c, c)
+	fmt.Printf("%d\n", a)
+	a = int(c)
+	fmt.Printf("%d\n", a)
 }
